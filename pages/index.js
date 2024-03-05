807e8723d347acf911cc445700d93442271af7dd
Aly <alyhassan2254@gmail.com>
2024-03-05 19:39:11 +0200

    added some fields

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,12 @@ export default function Home() {
     console.log(`${firstName} - ${lastName} - ${email} - ${phoneNumber}`);
   }
 
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
+  const [fullName, setFullName] = useState('')
+  const [age, setAge] = useState('')
   const [email, setEmail] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
+  const [grade, setGrade] = useState('')
+
   return (
     <form>
   <div className="grid gap-6 mb-6 mt-10 p-8 md:w-2/4">
@@ -18,16 +20,16 @@ export default function Home() {
         htmlFor="first_name"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
-        First name
+        Full name
       </label>
       <input
         type="text"
         id="first_name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="John"
+        placeholder="John Doe"
         required=""
-        onChange={(event) => setFirstName(event.target.value)}
-        value={firstName}
+        onChange={(event) => setFullName(event.target.value)}
+        value={fullName}
       />
     </div>
     <div>
@@ -35,16 +37,16 @@ export default function Home() {
         htmlFor="last_name"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
-        Last name
+        Age
       </label>
       <input
-        type="text"
-        id="last_name"
+        type="tel"
+        id="phone"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Doe"
+        placeholder="16"
         required=""
-        onChange={(event) => setLastName(event.target.value)}
-        value={lastName}
+        onChange={(event) => setAge(event.target.value)}
+        value={age}
       />
     </div>
     <div>
@@ -81,6 +83,24 @@ export default function Home() {
         value={phoneNumber}
       />
     </div>
+    <div>
+      <label
+        htmlFor="phone"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        Grade
+      </label>
+      <input
+        type="tel"
+        id="phone"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="8"
+        required=""
+        onChange={(event) => setGrade(event.target.value)}
+        value={grade}
+      />
+    </div>
+
     <button
     onClick={update}
     className="mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
